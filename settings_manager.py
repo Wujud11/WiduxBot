2210c376dc8025cac565f3799ae948d62f42d314
Wujud11 <4owj13@gmail.com>
2025-04-12 19:54:57 +0300

    Update settings_manager.py

--- settings_manager.py
+++ settings_manager.py
@@ -1,5 +1,3 @@
-# bot/settings_manager.py
-
 import json
 import os
 
@@ -20,6 +18,11 @@ class BotSettings:
                 "bot_username": "",
                 "access_token": "",
                 "mention_responses": [],
+                "mention_guard_duration": 86400,
+                "mention_guard_cooldown": 5,
+                "mention_guard_warning_thresh": 2,
+                "mention_guard_warn_msg": "لا تصايح كل شوي، انتبه!",
+                "mention_guard_timeout_msg": "تايم آوت بسيط، عشان تحترم نفسك شوي."
             }
 
     def save_settings(self):
@@ -35,3 +38,45 @@ class BotSettings:
 
     def get_all_settings(self):
         return self.settings
+
+    def update_bot_settings(self, form_data):
+        keys = [
+            "bot_username", "access_token", "mention_guard_duration",
+            "mention_guard_cooldown", "mention_guard_warning_thresh",
+            "mention_guard_warn_msg", "mention_guard_timeout_msg"
+        ]
+        for key in keys:
+            value = form_data.get(key)
+            if value is not None:
+                if key in ["mention_guard_duration", "mention_guard_cooldown", "mention_guard_warning_thresh"]:
+                    value = int(value)
+                self.update_setting(key, value)
+
+    def add_channel(self, channel_name):
+        if "channels" not in self.settings:
+            self.settings["channels"] = []
+        if channel_name not in self.settings["channels"]:
+            self.settings["channels"].append(channel_name)
+            self.save_settings()
+
+    def delete_channel(self, channel_name):
+        if "channels" in self.settings and channel_name in self.settings["channels"]:
+            self.settings["channels"].remove(channel_name)
+            self.save_settings()
+
+    def get_channel_settings(self, channel_name):
+        return {
+            "bot_username": self.settings.get("bot_username", ""),
+            "access_token": self.settings.get("access_token", "")
+        }
+
+    def update_channel_settings(self, channel_name, form_data):
+        # تحديث الإعدادات العامة (ما عندنا إعدادات خاصة لكل قناة حالياً)
+        self.update_bot_settings(form_data)
+
+    def update_custom_responses(self, form_data):
+        responses = {}
+        for key in form_data:
+            responses[key] = form_data[key].split("\n")
+        self.settings["custom_responses"] = responses
+        self.save_settings()
