dda5bc7777a4a0e18f5b7dfd82449713e97d167e
Wujud11 <4owj13@gmail.com>
2025-04-18 09:32:14 +0300

    Update settings_manager.py

--- settings_manager.py
+++ settings_manager.py
@@ -18,51 +18,27 @@ class BotSettings:
                 "access_token": "",
                 "channels": [],
                 "mention_limit": 2,
-                "mention_guard_warn_msg": "",
-                "mention_guard_timeout_msg": "",
-                "mention_guard_duration": 5,
+                "mention_guard_warn_msg": "ترى ببلعك تايم أوت",
+                "mention_guard_timeout_msg": "القم! أنا حذرتك",
+                "mention_guard_duration": 3,
                 "mention_guard_cooldown": 86400,
                 "mention_daily_cooldown": True,
                 "mention_responses": [],
                 "special_responses": {},
-                "custom_responses": {
-                    "solo_win_responses": [],
-                    "team_win_responses": [],
-                    "team_lose_responses": [],
-                    "group_win_responses": [],
-                    "group_lose_responses": [],
-                    "below_50_responses": [],
-                    "below_zero_responses": [],
-                    "doomed_leader_responses": [],
-                    "weak_leader_responses": [],
-                    "kicked_responses": [],
-                    "stolen_responses": []
-                },
+                "custom_responses": {},
                 "questions": []
             }
-            self.save_settings()
 
     def save_settings(self):
         with open(SETTINGS_FILE, "w", encoding="utf-8") as f:
             json.dump(self.settings, f, ensure_ascii=False, indent=2)
 
+    def get_setting(self, key):
+        return self.settings.get(key)
+
     def update_setting(self, key, value):
         self.settings[key] = value
         self.save_settings()
 
-    def get_setting(self, key):
-        return self.settings.get(key)
-
     def get_all_settings(self):
         return self.settings
-
-    def update_bot_settings(self, form_data):
-        keys = [
-            "bot_username", "access_token", "mention_limit",
-            "mention_guard_warn_msg", "mention_guard_timeout_msg",
-            "mention_guard_duration", "mention_guard_cooldown",
-            "mention_daily_cooldown"
-        ]
-        for key in keys:
-            if key in form_data:
-                self.update_setting(key, form_data[key])
